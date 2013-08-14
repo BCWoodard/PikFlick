73b6304f25b88e2022c2601dd6bb53664c6463e5
Brad Woodard <bcwoodard@gmail.com>
2013-08-14 16:54:39 -0500

    Copied VC.m code into PikFlick

--- PikFlick/ViewController.m
+++ PikFlick/ViewController.m
@@ -3,13 +3,17 @@
 //  PikFlick
 //
 //  Created by Brad Woodard on 8/14/13.
-//  Copyright (c) 2013 Brad Woodard. All rights reserved.
 //
 
 #import "ViewController.h"
 #import "Movie.h"
 
 @interface ViewController ()
+{
+    NSArray                     *moviesArray;
+    NSArray                     *moviePostersArray;
+    __weak IBOutlet UITableView *moviesTable;
+}
 
 @end
 
@@ -17,14 +21,142 @@
 
 - (void)viewDidLoad
 {
+    [[NSNotificationCenter defaultCenter]
+     addObserver:self
+     selector:@selector(getMovieGenre:)
+     name:@"GenreFound"
+     object:nil];
+    
+    [[NSNotificationCenter defaultCenter]
+     addObserver:self
+     selector:@selector(getPosterThumbnail:)
+     name:@"ThumbnailFound"
+     object:nil];
+    
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self getRottenTomatoesDATA];
+    
+}
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    if (motion == UIEventSubtypeMotionShake) {
+        NSLog(@"Did shake");
+    }
+}
+
+- (BOOL)canBecomeFirstResponder {
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (int)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (int)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [moviesArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"Cell"];
+    }
+    
+    // Grab movie object and poster thumbnail from their respective arrays
+    Movie *movie = [moviesArray objectAtIndex:indexPath.row];
+    //UIImage *moviePosterThumbnail = [moviePostersArray objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = movie.movieTitle;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%@. Genre: %@. Peer Rating: %@", movie.movieMPAA, movie.movieGenre, movie.moviePeerRating];
+    cell.imageView.image = movie.movieThumbnail;
+    
+    return cell;
+}
+
+- (void)getRottenTomatoesDATA
+{
+    // Activate the Network Activity Indicator
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+    
+    NSURL *url = [NSURL URLWithString:@"http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?page_limit=32&page=1&country=us&apikey=xx88qet7sppj6r7jp7wrnznd"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+        
+        // Retrieve all the Rotten Tomatoes movie data
+        NSDictionary *rottenTomatoesJSON = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+        
+        // Create an array of movie data and 2 temp arrays -
+        // One to hold movie objects - tempArray
+        // One to hold poster images - tempPostersArray
+        NSArray *dataMovieArray = [rottenTomatoesJSON objectForKey:@"movies"];
+        NSMutableArray *tempArray = [NSMutableArray arrayWithCapacity:[dataMovieArray count]];
+        // NSMutableArray *tempPostersArray = [NSMutableArray arrayWithCapacity:[dataMovieArray count]];
+        
+        for (NSDictionary *dictionary in dataMovieArray) {
+            // Create a movie using our init override method in Movie.m
+            Movie *movie = [[Movie alloc] initWithMovieDictionary:dictionary];
+            /*
+             NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:movie.movieThumbnailURL]];
+             UIImage *moviePosterThumbnail = [UIImage imageWithData:data];
+             [tempPostersArray addObject:moviePosterThumbnail];
+             */
+            [tempArray addObject:movie];
+        }
+        
+        // Populate our NSArrays with temporary mutable arrays
+        // Again, we do this to protect our arrays from accidental edits, etc.
+        //moviePostersArray = [NSArray arrayWithArray:tempPostersArray];
+        moviesArray = [NSArray arrayWithArray:tempArray];
+        
+        
+        
+        // Stop NetworkActivityIndicator
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+        
+        [moviesTable reloadData];
+        
+    }];
+}
+
+
+- (void)getMovieGenre:(NSNotification *)note
+{
+    Movie *movie = note.object;
+    
+    NSUInteger movieIndex = [moviesArray indexOfObject:movie];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:movieIndex inSection:0];
+    
+    [moviesTable reloadRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+
+
+- (void)getPosterThumbnail:(NSNotification *)note
+{
+    Movie *movie = note.object;
+    
+    NSUInteger movieIndex = [moviesArray indexOfObject:movie];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:movieIndex inSection:0];
+    
+    [moviesTable reloadRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:@"GenreFound" object:nil];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:@"ThumbnailFound" object:nil];
 }
 
 @end
