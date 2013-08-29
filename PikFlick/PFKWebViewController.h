cf8af6854a549421945e3f3e4a5d8609371e5f56
Brad Woodard <bcwoodard@gmail.com>
2013-08-28 19:56:06 -0500

    Added webview controller

---
+++ PikFlick/PFKWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  PFKWebViewController.h
+//  PikFlick
+//
+//  Created by Brad Woodard on 8/28/13.
+//  Copyright (c) 2013 Brad Woodard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PFKWebViewController : UIViewController
+
+@end
---
+++ PikFlick/PFKWebViewController.m
@@ -0,0 +1,38 @@
+//
+//  PFKWebViewController.m
+//  PikFlick
+//
+//  Created by Brad Woodard on 8/28/13.
+//  Copyright (c) 2013 Brad Woodard. All rights reserved.
+//
+
+#import "PFKWebViewController.h"
+
+@interface PFKWebViewController ()
+
+@end
+
+@implementation PFKWebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
